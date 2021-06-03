1079fbe505bc3d7f31af229ebf8a9d188e6b3835
Orazov Tairlan <llollus43@gmail.com>
2021-06-03 16:10:52 +0600

    Update

--- src/index.js
+++ src/index.js
@@ -1,123 +1,239 @@
+// import _ from "underscore";
+// import "./style.css";
+//
+// import jsonData from "../db.json";
+// import Lodash from "lodash";
+//
+// function loadItems() {
+//     const $table = document.querySelector(".iksweb");
+//     const json = groupJson(); // отфильтрованный массив объектов
+//     const html = json.map((item) => {
+//         return renderItems(item);
+//     });
+//     $table.insertAdjacentHTML("afterend", html.join(""));
+// }
+//
+// function renderItems(item) {
+//     let html = ``;
+//     let largeColumn = ``;
+//     for (let key in item) {
+//         if (key == "name") {
+//             largeColumn = (`
+//             <div class="largeColumn">
+//                 ${item.name}
+//             </div>`)
+//             continue;
+//         }
+//         html += (`
+//          <div class="twospan">
+//             <div class="half-span">
+//             ${item[key]}
+//             </div>
+//             <div class="half-span">
+//              ${item[key]}
+//             </div>
+//           </div>`);
+//     }
+//     let readyHTML = (`
+//         <div class="row">
+//         ${largeColumn}
+//         ${html}
+//         </div>`);
+//     return readyHTML;
+// }
+//
+// // фильтрует массив с объектами
+// function groupJson() {
+//     let arr = [];
+//     let arrResult = [];
+//     const json = jsonData;
+//     const grouppedData = _.groupBy(json, "name");
+//     let obj = copyObjNull(json);
+//     for (let key in grouppedData) {
+//         for (let i = 0; i < grouppedData[key].length; i++) {
+//             if (isMore(grouppedData[key], 1)) {
+//                 for (let prop in grouppedData[key][i]) {
+//                     if (typeof grouppedData[key][i][prop] == "number") {
+//                         obj[prop] += grouppedData[key][i][prop];
+//                     } else {
+//                         obj[prop] = grouppedData[key][i][prop];
+//                     }
+//                 }
+//                 let copyObj = {...obj};
+//                 arr.push(copyObj);
+//                 if (grouppedData[key].length - 1 == i) {
+//                     obj = copyObjNull(json);
+//                 }
+//             } else {
+//                 for (let prop in grouppedData[key][i]) {
+//                     obj[prop] = grouppedData[key][i][prop];
+//                 }
+//                 let copyObj = {...obj};
+//                 arr.push(copyObj);
+//                 obj = copyObjNull(json);
+//             }
+//         }
+//     }
+//     arr = _.groupBy(arr, "name");
+//     for (let key in arr) {
+//         let maxElement = _.last(arr[key]);
+//         arrResult.push(maxElement);
+//     }
+//     return arrResult;
+// }
+//
+// // создает копию объекта, но значения null
+// function copyObjNull(json) {
+//     let obj = {};
+//     for (let item of json) {
+//         for (let key in item) {
+//             obj[key] = null;
+//         }
+//     }
+//     return obj;
+// }
+//
+// function isMore(arr, length) {
+//     if (arr.length > length) {
+//         return true;
+//     }
+//     return false;
+// }
+//
+// let obj = {
+//     'a':  1,
+//     'b':  2,
+//     'c':  3,
+//     'cc': {
+//         'a':  1,
+//         'b':  2,
+//     }
+// }
+//
+// console.log(nestedLoop(obj));
+//
+//
+// // Рендерит дополнительные элементы
+// function renderMiscHTML(obj) {
+//     let row = ``;
+//     for (let key in obj) {
+//         console.log(obj);
+//         row += `
+//         <tr>
+//           <td>${obj[key]}</td>
+//         </tr>
+//         `;
+//     }
+//     let html = `
+//           <tbody>${row}</tbody>
+//         `;
+//     console.log(html);
+//     return html;
+// }
+//
+// loadItems();
+
 import _ from "underscore";
 import "./style.css";
 
 import jsonData from "../db.json";
 import Lodash from "lodash";
+import group from "underscore/modules/_group";
 
-// async function fetchData(path) {
-//     let result = await fetch(path);
-//     result = await result.json();
-//     console.log(result)
-//     return result;
-// }
-
-async function loadItems() {
+function loadItems() {
     const $table = document.querySelector(".headerRow");
-    const json = groupJson();
+    const json = groupJson(); // отфильтрованный массив объектов
     const html = json.map((item) => {
         return renderItems(item);
     });
-    $table.insertAdjacentHTML("afterend", html.join(""));
+    $table.insertAdjacentHTML("beforeend", html.join(""));
 }
 
-async function filterItems() {
-    const jsonData = jsonData;
-    let date = jsonData[0].date;
-    const result = jsonData.every((item) => {
-        return item.date == date;
-    });
-    return result;
+function renderItems(item){
+    return (`
+    <table class="iksweb">
+  <tbody>
+    <tr>
+      <td rowspan="5">Г</td>
+      <td rowspan="3">Ж</td>
+      <td>Д</td>
+      <td>К</td>
+    </tr>
+    <tr>
+      <td>Д</td>
+      <td>К</td>
+    </tr>
+    <tr>
+      <td>Д</td>
+      <td>К</td>
+    </tr>
+    <tr>
+      <td rowspan="2">М</td>
+      <td>Д</td>
+      <td>К</td>
+    </tr>
+    <tr>
+      <td>Д</td>
+      <td>К</td>
+    </tr>
+  </tbody>
+</table>;
+
+
+    `)
 }
 
-function iterateObj(obj) {
-    let html = "";
-    for (let key in obj) {
-        if (key == "female" || key == "male"){
-            html += `${key}:${obj[key]} <br>`
-        }
-        if (key !== "female" && key !== "male") {
-            console.log(typeof key);
-            html += `${key}:${obj[key]} <br>`;
-        }
-    }
-    return html;
-}
-
-function renderItems(item) {
-    const html = iterateObj(item);
-    return html;
-    // const html = item.return(
-   //      `
-   // <tbody class="row" data-name = ${item.name}>
-   //   <tr>
-   //       <td rowspan="2" class="cityName">${item.name}</td>
-   //       <td class="maleSex">Мужчина</td>
-   //       <td class="date">${item.date}</td>
-   //       <td class="countMale">${item.male}</td>
-   //   </tr>
-   //   <tr>
-   //       <td class="femaleSex">Женщина</td>
-   //       <td class="date">${item.date}</td>
-   //       <td class="countFemale">${item.female}</td>
-   //   </tr>
-   //   <tr>
-   //   </tbody>`
-
-}
-
+// фильтрует массив с объектами
 function groupJson() {
-    let arr = [];
+    const jsonData = jsonData;
+    console.log(jsonData);
+    // let arr = [];
+    // let arrResult = [];
+    // const json = jsonData;
+    // const grouppedData = _.groupBy(json, "name");
+    // let obj = copyObjNull(json);
+    // for (let key in grouppedData) {
+    //     for (let i = 0; i < grouppedData[key].length; i++) {
+    //         if (isMore(grouppedData[key], 1)) {
+    //             for (let prop in grouppedData[key][i]) {
+    //                 if (typeof grouppedData[key][i][prop] == "number") {
+    //                     obj[prop] += grouppedData[key][i][prop];
+    //                 } else {
+    //                     obj[prop] = grouppedData[key][i][prop];
+    //                 }
+    //             }
+    //             let copyObj = {...obj};
+    //             arr.push(copyObj);
+    //             if (grouppedData[key].length - 1 == i) {
+    //                 obj = copyObjNull(json);
+    //             }
+    //         } else {
+    //             for (let prop in grouppedData[key][i]) {
+    //                 obj[prop] = grouppedData[key][i][prop];
+    //             }
+    //             let copyObj = {...obj};
+    //             arr.push(copyObj);
+    //             obj = copyObjNull(json);
+    //         }
+    //     }
+    // }
+    // arr = _.groupBy(arr, "name");
+    // for (let key in arr) {
+    //     let maxElement = _.last(arr[key]);
+    //     arrResult.push(maxElement);
+    // }
+    // return arrResult;
+}
+
+function copyObjNull(json) {
     let obj = {};
-    const json = jsonData;
-    const grouppedData = _.groupBy(json, "name");
     for (let item of json) {
         for (let key in item) {
             obj[key] = null;
         }
     }
-    for (let key in grouppedData) {
-        for (let i = 0; i < grouppedData[key].length; i++) {
-            if (grouppedData[key].length > 1) {
-                for (let prop in grouppedData[key][i]) {
-                    if (typeof grouppedData[key][i][prop] == "number") {
-                        obj[prop] += grouppedData[key][i][prop];
-                    } else {
-                        obj[prop] = grouppedData[key][i][prop];
-                    }
-                }
-                console.log(obj, i, grouppedData[key].length);
-                let copyObj = {...obj};
-                arr.push(copyObj);
-                if (grouppedData[key].length - 1 == i) {
-                    for (let item of json) {
-                        for (let key in item) {
-                            obj[key] = null;
-                        }
-                    }
-                }
-            } else {
-                for (let prop in grouppedData[key][i]) {
-                    obj[prop] = grouppedData[key][i][prop];
-                }
-                let copyObj = {...obj};
-                arr.push(copyObj);
-                for (let item of json) {
-                    for (let key in item) {
-                        obj[key] = null;
-                    }
-                }
-            }
-        }
-    }
-    let result = Lodash.uniqBy(arr, "name");
-    arr = _.groupBy(arr, "name");
-    let arrResult = [];
-    for (let key in arr) {
-        arrResult.push(arr[key][arr[key].length - 1]);
-    }
-    return arrResult;
+    return obj;
 }
-
 
 function isMore(arr, length) {
     if (arr.length > length) {
@@ -125,7 +241,3 @@ function isMore(arr, length) {
     }
     return false;
 }
-
-loadItems();
-
-
